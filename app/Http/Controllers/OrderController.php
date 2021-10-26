98062464e10fa9ea6684252ecc38af49d957de1b
Renedz21 <u17203330@utp.edu.pe>
2021-10-25 20:16:59 -0500

    fix security

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,9 @@ class OrderController extends Controller
 {
     public function show(Order $order)
     {
+
+        $this->authorize('author', $order);
+
         $items = json_decode($order->content);
         return view('orders.show', compact('order', 'items'));
     }
